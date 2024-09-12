dfb8441c11ead3e1b1a3d9bb546d5ed10f2670d8
jakeyboy <143824958+jakebyurne@users.noreply.github.com>
2024-09-12 12:31:33 -0300

    Add files via upload

--- coisa.js
+++ coisa.js
@@ -1,8 +1,10 @@
 const butao = document.getElementById('butao');
+const hamter = document.getElementById('hamter');
 var audio = document.getElementById('audio');
 butao.addEventListener('click', () => {
 
-document.getElementById("butao").style.backgroundColor = "purple";
+    document.getElementById("hamter").style.display = "block";
+document.getElementById("butao").style.backgroundColor = "black";
 audio.volume = 0.300;
 audio.play();
 });
